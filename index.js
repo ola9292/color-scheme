efdd30d63b275f32faf302e41e8f9d434aea4105
Abass Olamide <51725999+ola9292@users.noreply.github.com>
2022-10-16 07:45:16 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+
+let colorPicker = document.getElementById('color-picker')
+const colorBtn = document.getElementById('color-btn')
+const colorMode = document.getElementById('color-mode')
+const colorContainer = document.getElementById('color-container')
+let chosenColor;
+let chosenMode;
+
+
+colorMode.addEventListener('change',function(){
+    chosenMode = colorMode.value;
+    return chosenMode
+})
+
+colorPicker.addEventListener('change',function(){
+    chosenColor = colorPicker.value.slice(1)
+    return chosenColor
+})
+
+colorBtn.addEventListener('click',function(){
+    console.log(chosenColor,chosenMode)
+fetch(`https://www.thecolorapi.com/scheme/?hex=${chosenColor}&mode=${chosenMode}&count=6`)
+.then((response) => response.json())
+.then((data) => {
+    console.log(data.colors)
+    let colorScheme = data.colors.map(function(colorStr){
+        return `
+        <div class="color-scheme-container">
+            <img src="${colorStr.image.bare}" class="color-image"/>
+            <div class="color">${colorStr.hex.value}</div>
+        </div>
+                `
+    }).join('')
+    colorContainer.innerHTML = colorScheme
+});
+})
+
+
